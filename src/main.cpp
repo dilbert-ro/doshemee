4e11c30c48f3a69de4bbfddb0b93bcd122866d47
cristimirca <81436569+cristimirca@users.noreply.github.com>
2021-05-12 20:09:12 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,18 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 600), "Pong clone");
-    const sf::Color MY_GREEN{0,128,0};
-    sf::RectangleShape teren(sf::Vector2f(800.f, 600.f));
-    sf::RectangleShape paleta1(sf::Vector2f(20.f, 100.f));
-    sf::RectangleShape paleta2(sf::Vector2f(20.f, 100.f));
-    sf::RectangleShape minge(sf::Vector2f(30.f, 30.f));
-    sf::Vertex fileu[] = 
-    {
-        sf::Vertex(sf::Vector2f(400.f, 0.f)),
-        sf::Vertex(sf::Vector2f(400.f, 600.f)),
-    };
+    sf::RectangleShape leftPaddleen(sf::Vector2f(20.f, 100.f));
+    sf::RectangleShape rightPaddleen(sf::Vector2f(20.f, 100.f));
+    sf::RectangleShape ball(sf::Vector2f(30.f, 30.f));
+    sf::Vertex fileu[] =
+        {
+            sf::Vertex(sf::Vector2f(400.f, 0.f)),
+            sf::Vertex(sf::Vector2f(400.f, 600.f)),
+        };
     sf::CircleShape cerc_mijloc(100.f);
+    const sf::Color MY_GREEN{0, 128, 0};
     sf::Font font;
-    if (!font.loadFromFile("/es/ARCADE.TTF"))
+    if (!font.loadFromFile("/res/ARCADE.TTF"))
     {
         if (!font.loadFromFile("output/res/ARCADE.TTF"))
         {
@@ -32,20 +31,17 @@ int main()
     scor_stanga.setCharacterSize(48);
     scor_stanga.setPosition(200.f, 0.f);
     scor_stanga.setStyle(sf::Text::Bold);
-    scor_dreapta.setString("2");
+    scor_dreapta.setString("0");
     scor_dreapta.setCharacterSize(48);
     scor_dreapta.setPosition(600.f, 0.f);
     scor_dreapta.setStyle(sf::Text::Bold);
-    paleta1.setPosition(0,250);
-    paleta2.setPosition(780,250);
-    minge.setPosition(200,200);
-    cerc_mijloc.setOrigin(-300,-200);
-    teren.setFillColor(MY_GREEN);
-    paleta1.setFillColor(sf::Color::White);
-    paleta2.setFillColor(sf::Color::White);
-    minge.setFillColor(sf::Color::White);
+    leftPaddleen.setPosition(0.f, 250.f);
+    rightPaddleen.setPosition(780.f, 250.f);
+    ball.setPosition(200.f, 150.f);
+    cerc_mijloc.setPosition(300, 200);
     cerc_mijloc.setFillColor(MY_GREEN);
     cerc_mijloc.setOutlineThickness(1);
+
     // run the program as long as the window is open
     while (window.isOpen())
     {
@@ -57,21 +53,38 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-     
-        window.clear();
-        window.draw(teren);
-        window.draw(paleta1);
-        window.draw(paleta2);
-        window.draw(minge);
+        //miscarea paleta stanga cu tastele A si Q
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        {
+            if (leftPaddleen.getPosition().y < 600 - leftPaddleen.getSize().y)
+                leftPaddleen.move(0.f, 0.1f);
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
+        {
+            if (leftPaddleen.getPosition().y > 0)
+                leftPaddleen.move(0.f, -0.1f);
+        }
+        //miscarea paleta dreapta cu tastele P si L
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::L))
+        {
+            if (rightPaddleen.getPosition().y < 600 - rightPaddleen.getSize().y)
+                rightPaddleen.move(0.f, 0.1f);
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::P))
+        {
+            if (rightPaddleen.getPosition().y > 0)
+                rightPaddleen.move(0.f, -0.1f);
+        }
+        window.clear(MY_GREEN);
+        window.draw(leftPaddleen);
+        window.draw(rightPaddleen);
+        window.draw(ball);
         window.draw(cerc_mijloc);
         window.draw(fileu, 2, sf::Lines);
-        window.draw(scor_stanga);
         window.draw(scor_dreapta);
-        
-        // draw everything here...
-        // window.draw(...);
-
-        // end the current frame
+        window.draw(scor_stanga);
         window.display();
     }
 
